014cbb1093b25b6bcfbe54f2c719b3aec4c51a12
sreelekha009 <sreelekhaediga09@gmail.com>
2023-10-25 23:44:30 +0530

    first commit

---
+++ task6/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './App.css';
+import Portfolio from './Portfolio';
+
+ 
+
+function App() {
+
+  return (
+
+    <div className="App">
+
+      <header>
+
+       
+
+      </header>
+
+      <main>
+
+        <Portfolio />
+
+      </main>
+
+    </div>
+
+  );
+
+}
+
+ 
+
+export default App;
---
+++ task6/src/Portfolio.js
@@ -0,0 +1,167 @@
+import React from 'react';
+
+ 
+
+const Portfolio = () => (
+
+  <div className="portfolio">
+
+    <div className="header">
+
+      <h1>Sreelekha Ediga</h1>
+
+      <h2>Student VIT AP</h2>
+
+      <div className="contact-info">
+
+        <h3>
+
+          <a href="mailto:sreelekhaediga09@gmail.com">sreelekhaediga09@gmail.com</a>
+
+        </h3>
+
+        <h3>8688601095</h3>
+
+      </div>
+
+    </div>
+
+ 
+
+    <div className="about">
+
+      <h2>About</h2>
+
+      <p className="enlarge">
+
+        I am VIT AP Student<br />
+
+        Born on 09 january 2004<br />
+
+        I have taken CSE (core)<br />
+
+        My registration number--21bce7030
+
+      </p>
+
+    </div>
+
+ 
+
+    <div className="education">
+
+      <h2>Education</h2>
+
+      <div className="education-item">
+
+        <h2>Little FLower School- Hyderabad, India</h2>
+
+        <p>10th class 9.2 GPA</p>
+
+      </div>
+
+      <div className="education-item">
+
+        <h2>Narayana- Hyderabad, India</h2>
+
+        <p>Inter 94%</p>
+
+      </div>
+
+    </div>
+
+ 
+
+    <div className="at-vit">
+
+      <h2>AT VIT AP</h2>
+
+      <p className="enlarge">
+
+        I am a third year student.
+
+      </p>
+
+    </div>
+
+ 
+
+    <div className="skills">
+
+      <h2>Skills</h2>
+
+      <div className="talent">
+
+        <h2>Java</h2>
+
+        <p>Core and Advanced java specialist</p>
+
+      </div>
+
+      <div className="talent">
+
+        <h2>JavaScript</h2>
+
+        <p>Can create complex web application front end scripting using JS and jQuery</p>
+
+      </div>
+
+      <div className="talent">
+
+        <h2>HTML</h2>
+
+        <p>Strong in writing HTML for UI design</p>
+
+        <br></br>
+
+        <br></br>
+
+      </div>
+
+    </div>
+
+    <div className="interests">
+
+      <h2>Interests and Hobbies</h2>
+
+      <ul className="talent">
+
+        <li>Reading Books</li>
+
+        <li>Playing Cricket</li>
+
+      </ul>
+
+      <ul className="talent">
+
+        <li>Coding</li>
+
+        <li>Watching Movies</li>
+
+      </ul>
+
+    </div>
+
+ 
+
+<br></br>
+
+<br></br>
+
+<br></br>
+
+<br></br>
+
+    <div className="footer">
+
+      <p>sreelekha Ediga &mdash; <a href="mailto:Sreelekhaediga09@gmail.com">sreelekhaediga09@gmail.com</a> &mdash; 8688601095</p>
+
+    </div>
+
+  </div>
+
+);
+
+ 
+
+export default Portfolio;
